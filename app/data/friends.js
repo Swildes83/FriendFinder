65b11e7dc15e8c2f9b28eda16f6329af10783eb8
Spencer Wildes <spencerwildes@gmail.com>
2018-01-11 21:56:43 -0800

    init commit

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friendsData = [
+  {
+    name: "Sir Patrick Stewart",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLvOiw73WjcWu2XhvIStsBqdTaDpl2SP_9eQZ4IUO23aPxV8bYmQ",
+    scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+  },
+  {
+    name: "Chuck Norris",
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkt-5PlcrHfLJ6GKAuQpeHiUfME99XMsTlhldGymB8OesErATx",
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  },
+  {
+    name: "Luke Skywalker",
+    photo:"http://filmjunkee.com/wp-content/uploads/2016/03/517dca96ad39a7214a41dbaab390d263-564x320.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Donald Trump",
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_FNPsPlMm00YsTLgMWa4FXdpUg4gu151MIp5Atuuj1n_QdGg6ww",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Frodo Baggins",
+    photo:"https://media.giphy.com/media/WeJCkUntqzjt6/giphy.gif",
+    scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+  },
+  {
+    name: "Lebron James",
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJrR5MfKa_oHhN_TB1WK3flMYEPjBmL8KNSgQ5kjJO4a2OkPBi",
+    scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+  }
+];
+
+
+
+// Export to API Route
+module.exports = friendsData;
